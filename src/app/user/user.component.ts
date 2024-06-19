01d55c761f86a053e2d7fa64d3aa255122b87c84
javieramezcua <javieramezcuaduran@gmail.com>
2024-06-19 00:03:16 -0600

    course progress june 18th

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,14 +7,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-  
-  get imagePath (){
-    return  'assets/users/' + this.selectedUser.avatar; 
+  @Input({ required:true }) avatar!: string;
+  @Input({ required:true }) name!: string;
+
+  get imagePath(){
+    return 'assets/users/'+this.avatar;
   }
 
   onSelectUser(){
-    console.log('clicked!')
   }
 }
